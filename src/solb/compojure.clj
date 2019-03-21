b1d6fd57a3ee165d962450df928fae630321d532
Solomon Bloch <solomonbloch@gmail.com>
2019-03-21 22:38:22 +0900

    user id showing off destructuring

--- src/solb/compojure.clj
+++ src/solb/compojure.clj
@@ -5,6 +5,8 @@
 
 (defroutes app
   (GET "/" [] "main page here!!")
+  (GET "/user/:id" [id] (str "hihihi " id))
+  (POST "/post" [:as req] (str req))
   (route/not-found "404 page"))
 
 (defonce server (jetty/run-jetty #'app {:port 5000 :join? false}))
